bebac673b3066c92c5518e509376da39075638d2
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2024-06-10 19:30:20 +0100

    embed field updates wokring, next to actually add in all the fields that need updating. probarbly a check so each field can only be added once

--- yagpdb/master reaction.go
+++ yagpdb/master reaction.go
@@ -1,54 +1,29 @@
 {{ $modmaillogo := "modmail:702099194701152266" }}
 {{ $msgID := .ReactionMessage.ID }}
 
-{{sendMessage nil "Saw reaction <.Reaction.APIName> get added to the message." }}
+{{sendMessage nil "Saw reaction <:.Reaction.APIName> get added to the message." }}
 
 {{ if eq .Reaction.Emoji.APIName $modmaillogo }}
-	{{sendMessage nil "Passed first check as provided reaction (<:.Reaction.APIName>) matches the expected reaction (<:modmail:702099194701152266>)." }}
-	{{ $embeds := .ReactionMessage.Embeds }}
-	{{ range $embed := $embeds }}
+	{{sendMessage nil "Passed first check as provided reaction (<:.Reaction.APIName) matches the expected reaction (<:modmail:702099194701152266>)." }}
+	{{ range .ReactionMessage.Embeds }}
+		{{ $embed := structToSdict . }}
+		{{ $embed.Set "Fields" (cslice.AppendSlice $embed.Fields) }}
 		{{ if eq $embed.Title "How do I setup ModMail?" }}
 			{{sendMessage nil "Passed second check as embed title matches the expected \"How do I setup ModMail?\"" }}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict 
+			{{ $embed.Set "Fields" ($embed.Fields.AppendSlice (cslice (sdict 
 				"name" "Advanced Setup" 
 				"value" "Some additional commands you could use are:" 
 				"inline" false
-			))}}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
+			) (sdict
 				"name" "`=pingrole <roles>`"
 				"value" "For configuring which roles get pinged upon a ModMail ticket being created."
 				"inline" true
-			))}}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
-				"name" "`=accessrole <roles>`"
-				"value" "For configuring which roles can reply to ModMail tickets."
-				"inline" true
-			))}}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
-				"name" "`=commandonly`"
-				"value" "For toggling if commands are required to reply to tickets.\nIf **disabled** staff have only to type in the channel for their message to be sent.\nIf **enabled** staff have to reply with `=reply` or `=areply`."
-				"inline" false
-			))}}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
-				"name" "`=anonymous`"
-				"value" "For toggling anonymous staff replies to hide the responder's name.\nThis does not work for making your end-user anonymous."
-				"inline" true
-			))}}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
-				"name" "`=logging`"
-				"value" "For toggling log messages of tickets being opened or closed.\nThis does not log a transcript of the messages."
-				"inline" true
-			))}}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
-				"name" "You can mention the roles, use role IDs or role names."
-				"value" "For role names with a space, it needs to be in quotes (e.g. \"Head Admin\")"
-				"inline" false
-			))}}
+			)))}}
 			{{editMessage nil $msgID (complexMessageEdit "embed" $embed)}}
 			{{ end }}
 		{{ if eq $embed.Title "How do I open a ticket?" }}
 			{{sendMessage nil "Passed second check as embed title matches the expected \"How do I open a ticket?\"" }}
-			{{ $embed.Set "fields" ($embed.fields.Append (sdict
+			{{ $embed.Set "fields" (cslice (sdict
 				"name" "Bonus Information: `=confirmation` command" 
 				"value" "If you have enabled the `=confirmation` mode, you will not be given the server selection menu immediately and will instead be prompted to resume messaging the last server you contacted.\nThis prompt will contain an option to take you to the server selection menu if it's incorrect but you can also use `=new <message>` to force the server selection menu to appear."
 				"inline" false
@@ -57,3 +32,29 @@
 			{{ end }}
 	{{ end}}
 {{ end }}
+
+(sdict
+	"name" "`=pingrole <roles>`"
+	"value" "For configuring which roles get pinged upon a ModMail ticket being created."
+	"inline" true
+) (sdict
+	"name" "`=accessrole <roles>`"
+	"value" "For configuring which roles can reply to ModMail tickets."
+	"inline" true
+) (sdict
+	"name" "`=commandonly`"
+	"value" "For toggling if commands are required to reply to tickets.\nIf **disabled** staff have only to type in the channel for their message to be sent.\nIf **enabled** staff have to reply with `=reply` or `=areply`."
+	"inline" false
+) (sdict
+	"name" "`=anonymous`"
+	"value" "For toggling anonymous staff replies to hide the responder's name.\nThis does not work for making your end-user anonymous."
+	"inline" true
+) (sdict
+	"name" "`=logging`"
+	"value" "For toggling log messages of tickets being opened or closed.\nThis does not log a transcript of the messages."
+	"inline" true
+) (sdict
+	"name" "You can mention the roles, use role IDs or role names."
+	"value" "For role names with a space, it needs to be in quotes (e.g. \"Head Admin\")"
+	"inline" false
+)
