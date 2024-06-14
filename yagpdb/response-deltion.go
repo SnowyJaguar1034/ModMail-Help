cbb14125cbdeef41fadef45d769325f14b7065a9
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2024-06-14 19:34:25 +0100

    fixing check, it became wrong at some point

--- yagpdb/response-deltion.go
+++ yagpdb/response-deltion.go
@@ -4,7 +4,7 @@
 {{ $bin := "bin:1250547674562957313" }}
 
 {{/* Checks if the reaction is the bin emoji */}}
-{{ if and (eq .Reaction.Emoji.APIName $bin) (ne .ReactionMessage.Author.ID .BotUser.ID) }}
+{{ if and (eq .Reaction.Emoji.APIName $bin) (eq .ReactionMessage.Author.ID .BotUser.ID) }}
 	{{editMessage nil $msgID (complexMessageEdit "content" "**Message will be deleted in 5 seconds**") }}
 	{{ deleteMessage nil $msgID 5 }}
 {{ end }}
