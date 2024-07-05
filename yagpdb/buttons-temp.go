e5c70590643171799a72f036fef8e3ba1fb6b0c9
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2024-07-05 19:32:44 +0100

    add initial buttons to replace reactions

---
+++ yagpdb/buttons-temp.go
@@ -0,0 +1,4 @@
+{{ $button := cbutton "label" "Delete" "custom_id" "support-response-delete" "style" 4 "emoji" (sdict "name" ":bin:1251255316121653343") }}
+{{ $button := cbutton "label" "Bookmark" "custom_id" "support-response-bookmark" "style" 2 "emoji" (sdict "name" ":bookmark:1251243802207846566") }}
+{{ $button := cbutton "label" "Show/Hide Extra Information" "custom_id" "support-response-toggle" "style" 1 "emoji" (sdict "name" ":mail:1251255870701047909") }}
+
