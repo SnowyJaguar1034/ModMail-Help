69b011583b949156b5f629bf63402f6f875a8c7b
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2024-07-05 21:28:35 +0100

    more buttons added

--- yagpdb/buttons-temp.go
+++ yagpdb/buttons-temp.go
@@ -1,4 +1,13 @@
-{{ $button := cbutton "label" "Delete" "custom_id" "support-response-delete" "style" 4 "emoji" (sdict "name" ":bin:1251255316121653343") }}
-{{ $button := cbutton "label" "Bookmark" "custom_id" "support-response-bookmark" "style" 2 "emoji" (sdict "name" ":bookmark:1251243802207846566") }}
-{{ $button := cbutton "label" "Show/Hide Extra Information" "custom_id" "support-response-toggle" "style" 1 "emoji" (sdict "name" ":mail:1251255870701047909") }}
+{{ $buttons := cslice }}
+{{ $deletebutton := cbutton "label" "Delete Response" "custom_id" "support-response-delete" "style" 4 "emoji" (sdict "id" "1251255316121653343") }}
+{{ $bookmarkbutton := cbutton "label" "Bookmark Response" "custom_id" "support-response-bookmark" "style" 2 "emoji" (sdict "id" "1251243802207846566") }}
+{{ $togglebutton := cbutton "label" "Show/Hide Extra Information" "custom_id" "support-response-toggle" "style" 1 "emoji" (sdict "id" "1258858981372330165") }}
+{{ $premiumbutton := cbutton "label" "Buy Premium" "custom_id" "support-response-premium" "url" "https://modmail.xyz/premium" "style" "link" "emoji" (sdict "id" "1251273319110414429") }}
+{{ $invitebutton := cbutton "label" "Invite ModMail" "custom_id" "support-response-invite" "url" "https://modmail.xyz/invite" "style" "link" "emoji" (sdict "id" "1251255870701047909") }}
+{{ $commandsbutton := cbutton "label" "ModMail Commands (`=help`)" "custom_id" "support-response-commands" "url" "https://modmail.xyz/commands" "style" "link""emoji" (sdict "id" "1258858466081116293") }}
+{{ $logsbutton := cbutton "label" "Example Logs" "custom_id" "support-response-logs" "url" "https://modmail.xyz/logs/d7586c153425000-10d1416086c01033-10d141608b802047" "style" "link" "emoji" (sdict "id" "1254891424965722122") }}
+{{ idguidebutton := cbutton "label" "Discord ID Guide" "custom_id" "support-response-idguide" "url" "https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-" "style" "link" "emoji" (sdict "id" "1258860561869832246") }}
 
+{{ $buttons = $buttons.Append $deletebutton $bookmarkbutton $togglebutton }}
+{{ $message := complexMessage "buttons" $buttons }}
+{{ sendMessage nil $message }}
